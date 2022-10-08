6dff19b47532f2113be4652f670fdec66c597b1c
E41210960_Rio Javier Reyhan_B <99681374+RioJavier15@users.noreply.github.com>
2022-10-08 19:34:24 +0700

    Add files via upload

---
+++ model/Chart_model.dart
@@ -0,0 +1,101 @@
+class ChartModel {
+  final String? name;
+  final String? message;
+  final String? time;
+  final String? profileUrl;
+
+  ChartModel({this.name, this.message, this.time, this.profileUrl});
+}
+
+final List<ChartModel> items = [
+  ChartModel(
+      name: 'renata',
+      message: 'Hello chef',
+      time: '19.00',
+      profileUrl:
+          'https://asset.kompas.com/crops/IjmmRwybxe-4tZrGRN5eMrq_po8=/0x32:700x382/750x500/data/photo/2020/01/09/5e16811892fc7.jpg'),
+  ChartModel(
+      name: 'rafli',
+      message: 'hello rafli',
+      time: '18.00',
+      profileUrl:
+          'https://img.okezone.com/content/2019/04/30/298/2049787/chef-arnold-berbagi-tips-membuat-makanan-menjadi-lebih-enak-Tz2kVDuNL2.jpg'),
+  ChartModel(
+      name: 'doni',
+      message: 'hello doni',
+      time: '17.00',
+      profileUrl:
+          'https://asset.kompas.com/crops/ployX7cQOqsYqJS2PYvUGv41CaI=/0x0:1000x667/750x500/data/photo/2017/06/22/163146320170622-042902-8311-chef.juna-.atau-.junior-.rorimpandey-.jpg'),
+  ChartModel(
+      name: 'zulfikar',
+      message: 'hello zul',
+      time: '7 oktober',
+      profileUrl:
+          'https://img-o.okeinfo.net/content/2020/04/23/298/2203898/memilih-menu-sehat-dan-bergizi-saat-ramadhan-ala-chef-degan-khJm4HN40z.jpg'),
+  ChartModel(
+      name: 'faros',
+      message: 'hello faros',
+      time: '6 oktober',
+      profileUrl:
+          'https://img-o.okeinfo.net/content/2020/04/23/298/2203898/memilih-menu-sehat-dan-bergizi-saat-ramadhan-ala-chef-degan-khJm4HN40z.jpg'),
+  ChartModel(
+      name: 'apis',
+      message: 'hello apis',
+      time: '5 oktober',
+      profileUrl:
+          'https://img-o.okeinfo.net/content/2020/04/23/298/2203898/memilih-menu-sehat-dan-bergizi-saat-ramadhan-ala-chef-degan-khJm4HN40z.jpg'),
+  ChartModel(
+      name: 'akbar',
+      message: 'hello akbar',
+      time: '4 oktober',
+      profileUrl:
+          'https://img-o.okeinfo.net/content/2020/04/23/298/2203898/memilih-menu-sehat-dan-bergizi-saat-ramadhan-ala-chef-degan-khJm4HN40z.jpg'),
+  ChartModel(
+      name: 'niva',
+      message: 'hello niva',
+      time: '12.21',
+      profileUrl:
+          'https://img-o.okeinfo.net/content/2020/04/23/298/2203898/memilih-menu-sehat-dan-bergizi-saat-ramadhan-ala-chef-degan-khJm4HN40z.jpg'),
+  ChartModel(
+      name: 'citra',
+      message: 'hello citra',
+      time: '13.41',
+      profileUrl:
+          'https://img-o.okeinfo.net/content/2020/04/23/298/2203898/memilih-menu-sehat-dan-bergizi-saat-ramadhan-ala-chef-degan-khJm4HN40z.jpg'),
+  ChartModel(
+      name: 'adi',
+      message: 'hello adi',
+      time: '25 january',
+      profileUrl:
+          'https://img-o.okeinfo.net/content/2020/04/23/298/2203898/memilih-menu-sehat-dan-bergizi-saat-ramadhan-ala-chef-degan-khJm4HN40z.jpg'),
+  ChartModel(
+      name: 'ashraf',
+      message: 'hello ashraf',
+      time: '19.00',
+      profileUrl:
+          'https://img-o.okeinfo.net/content/2020/04/23/298/2203898/memilih-menu-sehat-dan-bergizi-saat-ramadhan-ala-chef-degan-khJm4HN40z.jpg'),
+  ChartModel(
+      name: 'niva',
+      message: 'hello niva',
+      time: '12.21',
+      profileUrl:
+          'https://img-o.okeinfo.net/content/2020/04/23/298/2203898/memilih-menu-sehat-dan-bergizi-saat-ramadhan-ala-chef-degan-khJm4HN40z.jpg'),
+  ChartModel(
+      name: 'citra',
+      message: 'hello citra',
+      time: '13.41',
+      profileUrl:
+          'https://img-o.okeinfo.net/content/2020/04/23/298/2203898/memilih-menu-sehat-dan-bergizi-saat-ramadhan-ala-chef-degan-khJm4HN40z.jpg'),
+  ChartModel(
+      name: 'adi',
+      message: 'hello adi',
+      time: '25 january',
+      profileUrl:
+          'https://img-o.okeinfo.net/content/2020/04/23/298/2203898/memilih-menu-sehat-dan-bergizi-saat-ramadhan-ala-chef-degan-khJm4HN40z.jpg'),
+  ChartModel(
+      name: 'ashraf',
+      message: 'hello ashraf',
+      time: '19.00',
+      profileUrl:
+          'https://img-o.okeinfo.net/content/2020/04/23/298/2203898/memilih-menu-sehat-dan-bergizi-saat-ramadhan-ala-chef-degan-khJm4HN40z.jpg'),
+];
